d2e95cf7aae7c6f1fe4a6c7d6863e13e2cdef1d1
Trung Trinh <trung_trinh@epam.com>
2026-01-28 08:47:37 -0500

    tf

--- terraform-tutorial/main.tf
+++ terraform-tutorial/main.tf
@@ -1,4 +1,17 @@
 provider "aws" {
   profile = "default"
-  region = "us-west-2"
+  region = "ca-central-1"
+}
+
+# See the docs for this provider here
+# 
+resource "aws_instance" "app_server"{
+  # Check here to see: AWS EC2 Console → Launch Instance → and check the AMI IDs shown for your region.
+  # https://ca-central-1.console.aws.amazon.com/ec2/home?region=ca-central-1#LaunchInstances:
+  ami = "ami-0a6dc9ac31a91b33c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyTerraformInstance"
+  }
 }
